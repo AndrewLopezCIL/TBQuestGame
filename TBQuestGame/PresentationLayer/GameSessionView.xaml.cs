f5996fde1e0b10e5935a9bc4e435ab42e89e0ba7
Andrew Lopez <andrewlopezcil@gmail.com>
2019-03-11 10:16:55 -0400

    Enemy push

--- TBQuestGame/PresentationLayer/GameSessionView.xaml.cs
+++ TBQuestGame/PresentationLayer/GameSessionView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using TBQuestGame.PresentationLayer;
 namespace TBQuestGame.PresentationLayer
 {
     /// <summary>
@@ -21,6 +22,7 @@ namespace TBQuestGame.PresentationLayer
     {
 
         GameSessionViewModel _gameSessionViewModel;
+        
         private string Messages;
 
         public GameSessionView(GameSessionViewModel gameSessionViewModel)
@@ -43,10 +45,45 @@ namespace TBQuestGame.PresentationLayer
 
         private void SkillsButton_Click(object sender, RoutedEventArgs e)
         {
+            ListBoxItem item = new ListBoxItem();
+            StackPanel new_item = new StackPanel();
+            new_item.Orientation = Orientation.Horizontal;
+            Image img = new Image();
+            BitmapImage bitImage = new BitmapImage();
+            bitImage.BeginInit();
+            bitImage.UriSource = new Uri("/Images/warrior-icon.png", UriKind.Relative);
+            bitImage.EndInit();
+            img.Source = bitImage;
+            img.Width = 32;
+            img.Height = 32;
+
+            TextBlock entityLevel = new TextBlock();
+            entityLevel.Text = "{LVL 55}";
+            entityLevel.FontWeight = FontWeights.Bold;
+            entityLevel.FontSize = 16;
+            entityLevel.VerticalAlignment = VerticalAlignment.Center;
+
+            TextBlock entityName = new TextBlock();
+            entityName.Text = "Warrior";
+            entityName.FontSize = 15.5;
+            entityName.FontWeight = FontWeights.Bold;
+            entityName.VerticalAlignment = VerticalAlignment.Center;
+            
+            new_item.Children.Add(img);
+            new_item.Children.Add(entityLevel);
+            new_item.Children.Add(entityName);
+            item.Content = new_item;
+            item.Background = Brushes.Red;
+            item.BorderBrush = Brushes.Black;
+            item.BorderThickness = new Thickness(3,3,3,0);
+            
+            ActiveEnemies.Items.Add(item);
+        }  
+        private static void AddEnemyToList()
+        {
 
         }
-
-        private void AttackButton_Click(object sender, RoutedEventArgs e)
+          private void AttackButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
